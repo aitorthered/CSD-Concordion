930b4d29cc9535cff7fbea245e8eefa255ce1984
Luis <luisrovirosa@gmail.com>
2013-05-22 12:54:45 +0200

    Refactoring

--- test/unit/com/example/service/UserServiceTest.java
+++ test/unit/com/example/service/UserServiceTest.java
@@ -18,56 +18,47 @@ import com.example.entity.User;
 import com.example.service.UserService;
 
 public class UserServiceTest {
+	private static final String USER_PASS = "mipass";
+	private static final String USER_EMAIL = "pepe@micorreo.es";
+	private static final String USER_NAME = "Pepe";
+	private static final String USER_USER_ID = "pepito";
 	UserDao dao = mock(UserDao.class);
 	UserService service = new UserService(dao);
 
 	@Test
 	public void testAddUserCallsDaoWithAnyUser() {
-		service.addUser("pepito", "Pepe", "pepe@micorreo.es", "mipass");
+		service.addUser(USER_USER_ID, USER_NAME, USER_EMAIL, USER_PASS);
 		verify(dao).insert((User) any());
 	}
 
 	@Test
 	public void testAddUserCallsDaoWithSpecificUser() {
-		service.addUser("pepito", "Pepe", "pepe@micorreo.es", "mipass");
-		User user = new User();
-		user.setName("Pepe");
-		user.setUserId("pepito");
-		user.setEmail("pepe@micorreo.es");
+		service.addUser(USER_USER_ID, USER_NAME, USER_EMAIL, USER_PASS);
+		User user = createUser();
 		verify(dao).insert(user);
 	}
 
 	@Test
 	public void testAddUserCallsDaoCallsPassword() {
-		service.addUser("pepito", "Pepe", "pepe@micorreo.es", "mipass");
-		User user = new User();
-		user.setName("Pepe");
-		user.setUserId("pepito");
-		user.setEmail("pepe@micorreo.es");
+		service.addUser(USER_USER_ID, USER_NAME, USER_EMAIL, USER_PASS);
 		verify(dao).setPassword((User) any(), anyString());
 	}
 
 	@Test
 	public void testAddUserCallsDaoXCallsPasswordWithSpecificUser() {
-		service.addUser("pepito", "Pepe", "pepe@micorreo.es", "mipass");
-		User user = new User();
-		user.setUserId("pepito");
-		user.setName("Pepe");
-		user.setEmail("pepe@micorreo.es");
-		verify(dao).setPassword(user, "mipass");
+		service.addUser(USER_USER_ID, USER_NAME, USER_EMAIL, USER_PASS);
+		User user = createUser();
+		verify(dao).setPassword(user, USER_PASS);
 	}
 
 	@Test
 	public void testAddUserCallsDaoXCallsPasswordInTheCorrectOrder() {
-		service.addUser("pepito", "Pepe", "pepe@micorreo.es", "mipass");
-		User user = new User();
-		user.setUserId("pepito");
-		user.setName("Pepe");
-		user.setEmail("pepe@micorreo.es");
+		service.addUser(USER_USER_ID, USER_NAME, USER_EMAIL, USER_PASS);
+		User user = createUser();
 
 		InOrder inOrder = inOrder(dao, dao);
 		inOrder.verify(dao).insert(user);
-		inOrder.verify(dao).setPassword(user, "mipass");
+		inOrder.verify(dao).setPassword(user, USER_PASS);
 	}
 
 	@Test
@@ -78,30 +69,32 @@ public class UserServiceTest {
 
 	@Test
 	public void testLoginWithUserIdAndPasswordCallsDao() {
-		service.canLogin("pepito", "mipass");
+		service.canLogin(USER_USER_ID, USER_PASS);
 		verify(dao).isValidPassword((User) any(), anyString());
 	}
 
 	@Test
 	public void testLoginWithUserIdAndPasswordCallsDaoWithCorrectUser() {
+		User user = createUser();
+		when(dao.findById(USER_USER_ID)).thenReturn(user);
+		service.canLogin(USER_USER_ID, USER_PASS);
+		verify(dao).isValidPassword(user, USER_PASS);
+	}
+
+	private User createUser() {
 		User user = new User();
-		user.setUserId("pepito");
-		user.setName("Pepe");
-		user.setEmail("pepe@micorreo.es");
-		when(dao.findById("pepito")).thenReturn(user);
-		service.canLogin("pepito", "mipass");
-		verify(dao).isValidPassword(user, "mipass");
+		user.setUserId(USER_USER_ID);
+		user.setName(USER_NAME);
+		user.setEmail(USER_EMAIL);
+		return user;
 	}
 
 	@Test
 	public void testLoginWithUserIdAndPasswordCallsDaoAndReturnSameValueThanDao() {
-		User user = new User();
-		user.setUserId("pepito");
-		user.setName("Pepe");
-		user.setEmail("pepe@micorreo.es");
-		when(dao.findById("pepito")).thenReturn(user);
-		when(dao.isValidPassword(user, "mipass")).thenReturn(true);
-		assertTrue(service.canLogin("pepito", "mipass"));
-		assertFalse(service.canLogin("pepito", "mipass2"));
+		User user = createUser();
+		when(dao.findById(USER_USER_ID)).thenReturn(user);
+		when(dao.isValidPassword(user, USER_PASS)).thenReturn(true);
+		assertTrue(service.canLogin(USER_USER_ID, USER_PASS));
+		assertFalse(service.canLogin(USER_USER_ID, "mipass2"));
 	}
 }
